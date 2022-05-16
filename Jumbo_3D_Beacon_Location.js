ad6bae59fd0d277be1e74e1b7e746784df747045
Arcgistest65 <104002712+Arcgistest65@users.noreply.github.com>
2022-05-16 15:09:15 +0400

    Update Jumbo_3D_Beacon_Location.js

--- Jumbo_3D_Beacon_Location.js
+++ Jumbo_3D_Beacon_Location.js
@@ -30,7 +30,6 @@ class Map extends HTMLElement {
         this.appendChild(template.content.cloneNode(true));
         this._props = {};
         let that = this;
-        locationData = this.$servicelevel; 
             
             
         require(
