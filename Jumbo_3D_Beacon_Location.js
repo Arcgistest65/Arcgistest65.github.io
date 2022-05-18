a60f29b7e1e14d2465c11a3a158ddf8876e49d85
Arcgistest65 <104002712+Arcgistest65@users.noreply.github.com>
2022-05-18 12:20:30 +0400

    Update Jumbo_3D_Beacon_Location.js

--- Jumbo_3D_Beacon_Location.js
+++ Jumbo_3D_Beacon_Location.js
@@ -138,9 +138,8 @@ function processbeacons() {
 
 //Convert JSON to GEOJSON
 
-function removeString(stringCoord){
-stringCoor=", "+stringCoord;
-var LatLng = stringCoor.replace("(", "").replace(")", "").split(", ")
+function removeString(stringCoor){
+var LatLng = stringCoor.replace("[", "").replace("]", "").split(", ")
 var Lat = parseFloat(LatLng[0]);
 var Lng = parseFloat(LatLng[1]);
 var x=parseFloat(LatLng[2]);
