ffaabdfe5c2b8ae1e1891790a837fb51c4373c23
Arcgistest65 <104002712+Arcgistest65@users.noreply.github.com>
2022-06-01 15:04:29 +0400

    Update Beacon_widget_builder.js

--- Beacon_widget_builder.js
+++ Beacon_widget_builder.js
@@ -4,7 +4,7 @@
       <style>
           fieldset {
               margin-bottom: 10px;
-              padding: 10px;
+              padding: 50px;
               border: 1px solid #afafaf;
               border-radius: 3px;
           }
