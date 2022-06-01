201008aa7a6de91f8adf0f0502035fafc86ea825
Arcgistest65 <104002712+Arcgistest65@users.noreply.github.com>
2022-06-01 15:19:12 +0400

    Update Beacon_widget_builder.js

--- Beacon_widget_builder.js
+++ Beacon_widget_builder.js
@@ -57,6 +57,10 @@
               <td><label for="layerOption">Layer Option:</label></td>
               <td><input id="layerOption" name="layerOption" type="text"></td>
             </tr>
+            <tr>
+              <td><label for="bgOption">Bg Option:</label></td>
+              <td><input id="bgOption" name="bgOption" type="text"></td>
+            </tr>
           </table>
         </fieldset>
         <button type="submit" hidden>Submit</button>
@@ -145,6 +149,12 @@
         set legendOption(value) {
             this.setValue("legendOption", value);    
         }
+         get bgOption() {
+            return this.getValue("bgOption");
+        }
+        set bgOption(value) {
+            this.setValue("bgOption", value);    
+        }
         
         getValue(id) {
             return this._shadowRoot.getElementById(id).value;
@@ -162,7 +172,8 @@
                 "StartSize",
                 "StopSize",
                 "legendOption",
-                "layerOption"
+                "layerOption",
+                "bgOption"
             ];
         }
 
